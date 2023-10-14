c2349f25f9673dfca0cf2e543c9ef6cfe4320ae5
mgXzyy <107469245+mgXzyy@users.noreply.github.com>
2023-10-14 12:33:26 +0300

    add request for elevation

--- winfontchanger/Program.cs
+++ winfontchanger/Program.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
 
 namespace winfontchanger
 {
@@ -14,10 +17,36 @@ namespace winfontchanger
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new mainform());
+            WindowsPrincipal pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool hasAdministrativeRight = pricipal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!hasAdministrativeRight)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.UseShellExecute = true;
+                startInfo.WorkingDirectory = Environment.CurrentDirectory;
+                startInfo.FileName = Application.ExecutablePath;
+                startInfo.Verb = "runas";
+                try
+                {
+                    Process p = Process.Start(startInfo);
+                    Application.Exit();
+                }
+                catch (System.ComponentModel.Win32Exception ex)
+                {
+                    MessageBox.Show("This utility requires elevated priviledges to complete correctly.", "Error: UAC Authorisation Required", MessageBoxButtons.OK);
+                    //                    Debug.Print(ex.Message);
+                    return;
+                }
+            }
+            else
+            {
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new mainform());
+            }
+            
         }
     }
 }
